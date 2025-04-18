7a57d325d509c56f95e0a4f7db0068d4e9e2beaf
Lucas Ariel Oliveira Moreira dos Santos <lariel.olim@gmail.com>
2025-04-18 12:04:30 -0300

    copy files and folders listed to backup folder

--- exec.sh
+++ exec.sh
@@ -9,7 +9,27 @@ if [[ -d $backup_dir_name ]]; then
     rm -rf $backup_dir_name
 fi
 
+# make the backup directory
 mkdir $backup_dir_name
+
+
+for i in $(cat list);
+do
+    if [[ !(-e $i) ]]; then
+        echo "============*WARNING*============"   
+        echo "[ $i ] does not exists, please check it!"
+        echo -e "================================="
+        continue
+    fi
+
+    if [[ -d $i ]]; then
+        cp -r $i $backup_dir_name
+        continue
+    fi
+
+    cp $i $backup_dir_name
+done
+
 
 if [[ -n $1 ]]; then
     echo "is not null"
